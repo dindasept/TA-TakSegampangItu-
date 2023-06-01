2137931a3c4a240d0c48fe3ae17e8ef81f83eb4d
dindasept <dindaseptianingsih09@gmail.com>
2023-06-01 20:54:01 +0700

    master : Upload Project

---
+++ Project/BrideTobe/app/src/main/java/com/uti/bridetobe/RegisterActivity.kt
@@ -0,0 +1,119 @@
+package com.uti.bridetobe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uti.bridetobe.ui.theme.BrideTobeTheme
+
+class RegisterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            setContent {
+                BrideTobeTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(modifier = Modifier.fillMaxSize().background(Color.LightGray)) {
+
+                    }
+                    LoginScreen()
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun LoginScreen() {
+        val (email, setEmail) = remember { mutableStateOf("") }
+        val (password, setPassword) = remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.bridetobelogo),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.clip(
+                    CircleShape
+                ).border(0.dp, Color.Gray, CircleShape)
+            )
+
+            Text(
+                text = " ",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            TextField(
+                value = email,
+                onValueChange = setEmail,
+                label = { Text("Username") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            TextField(
+                value = email,
+                onValueChange = setEmail,
+                label = { Text("Email") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            TextField(
+                value = password,
+                onValueChange = setPassword,
+                label = { Text("Password") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            Button(
+                onClick = { /* Handle login button click */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp), colors = ButtonDefaults.buttonColors(Color.Gray)
+            ) {
+                Text("Sign Up")
+            }
+        }
+    }
+}
+
